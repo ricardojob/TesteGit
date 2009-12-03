46f24dfe459d32a85bffbb32b5f9b47f2106be03
ricardojob <sousajob@gmail.com>
2009-12-03 02:55:05 -0200

    Enviando direto pelo Netbeans

--- src/outro/Agora.java
+++ src/outro/Agora.java
@@ -1,11 +1,11 @@
 package outro;
 
-
 public class Agora {
 
     public static void main(String[] args) {
         System.out.println("Ricardo Job");
         System.out.println("Outro teste");
         System.out.println("Outro teste");
+        System.out.println("Meiii");
     }
 }
