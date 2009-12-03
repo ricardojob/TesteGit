5431fcd5d15a100e67a442922eb5931904ab4290
ricardojob <sousajob@gmail.com>
2009-12-03 16:18:16 -0200

    Atualizacao na aula

--- src/outro/Agora.java
+++ src/outro/Agora.java
@@ -3,10 +3,9 @@ package outro;
 public class Agora {
 
     public static void main(String[] args) {
+
         System.out.println("Ricardo Job");
         System.out.println("Outro teste");
-        System.out.println("Outro teste");
-        System.out.println("Duas e 20");
-
+        System.out.println("Outro teste"); 
     }
 }
