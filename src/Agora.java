ec21825238f9435013769ec61161b4ffa0399d0a
Ricardo Job <sousajob@gmail.com>
2009-12-02 22:04:55 -0200

    Iniciando pelo Netbeans

--- src/Agora.java
+++ src/Agora.java
@@ -1,7 +1,14 @@
 
 public class Agora {
+
     public static void main(String[] args) {
-    System.out.println("Ricardo Job");
+        System.out.println("Ricardo Job");
+
+
+        
+
+
+
+        System.out.println("Outro teste");
     }
-    
 }
